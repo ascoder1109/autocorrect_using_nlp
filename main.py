a74dcbfe3ee3771d877ae3cf258bf5db177b5e5b
Aditya Singh <as1671109@gmail.com>
2024-02-23 14:08:04 +0530

    Done with algorithm

---
+++ main.py
@@ -0,0 +1,19 @@
+import re
+import functions
+w = []
+with open('Data/final.txt','r',encoding="utf") as f:
+    fileName = f.read()
+    fileName = fileName.lower()
+    w = re.findall('\w+',fileName)
+
+mainSet = set(w)
+
+myWord = input("Enter any word:")
+wordCount = functions.countingWords(mainSet)
+probablities = functions.probabalityCalculate(wordCount)
+temporaryCorrections = functions.getCorrections(myWord,probablities,mainSet,2)
+for i, word_prob in enumerate(temporaryCorrections):
+    if(i < 3):
+        print(word_prob[0])
+    else:
+        break
